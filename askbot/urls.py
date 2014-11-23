3e7b5e4d3128151d31111ad62270ac9ec8ff7b8c
David Lu <davidvlu@gmail.com>
2014-11-23 15:50:17 -0600

    Add URL for answer in API

--- askbot/urls.py
+++ askbot/urls.py
@@ -621,6 +621,7 @@ urlpatterns = patterns('',
     service_url('^api/v1/users/(?P<user_id>\d+)/$', views.api_v1.user, name='api_v1_user'),
     service_url('^api/v1/questions/$', views.api_v1.questions, name='api_v1_questions'),
     service_url('^api/v1/questions/(?P<question_id>\d+)/$', views.api_v1.question, name='api_v1_question'),
+    service_url('^api/v1/answers/(?P<answer_id>\d+)/$', views.api_v1.answer, name='api_v1_answer'),
 )
 
 if 'askbot.deps.django_authopenid' in settings.INSTALLED_APPS:
